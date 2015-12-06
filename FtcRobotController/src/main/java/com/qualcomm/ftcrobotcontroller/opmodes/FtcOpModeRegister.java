f395a87eaead0aa0f2c7e8d22f60111922dc41af
Ryley <ryley.hindman@studentmail.tiseagles.com>
2015-12-05 18:39:03 -0600

    Auto Mode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -62,11 +62,10 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
     manager.register("CF_TracksManual", CF_TracksManual.class);
-<<<<<<< HEAD
+
     manager.register("CF_TracksAutonomous", CF_Auto_Test.class);
     manager.register("CF_AutoBlueMtn", CF_AutoBlueMtn.class);
-=======
->>>>>>> master
+
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
